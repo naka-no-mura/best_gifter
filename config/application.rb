de6889f4e7a11dc11149b58bd0bd3e77b3046945
naka-no-mura <nakanomura.ryu@gmail.com>
2021-01-01 20:09:50 +0900

    add:自動生成ファイルの制限

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,12 @@ module BestGifter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.generators do |g|
+      g.assets false
+      g.test_framework false
+      g.skip_routes true
+      g.helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
