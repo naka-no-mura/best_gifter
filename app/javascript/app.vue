a46b8d80ef73045d7abe5c80fc383893deb19835
naka-no-mura <nakanomura.ryu@gmail.com>
2021-01-02 16:17:05 +0900

    add:header

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,8 +1,21 @@
 <template>
-  <router-view />
+  <div class="d-flex flex-column min-vh-100">
+    <TheHeader class="mb-auto" />
+    <router-view />
+    <!-- <TheFooter class="mt-auto" /> -->
+  </div>
 </template>
 
 <script>
+  import TheHeader from 'components/TheHeader.vue'
+  // import TheFooter from 'components/TheFooter.vue'
+
+  export default {
+    components: {
+      TheHeader,
+      // TheFooter,
+    }
+  };
 </script>
 
 <style scoped>
--- app/javascript/components/TheHeader.vue
+++ app/javascript/components/TheHeader.vue
@@ -1,7 +1,39 @@
 <template>
+  <header class="mb-auto">
+    <nav class="navbar fixed-top navbar-expand navbar-light bg-light justify-content-between">
+      <img v-bind:src="logo_img" width="8%" alt = "ロゴ" />
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">検索</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">相場とおすすめ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">新規登録</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ログイン</a>
+      </li>
+      <li class="nav-item">
+        <img v-bind:src="icon_img" width="" alt = "アイコン" width="200%" />
+      </li>
+    </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
+export default {
+  name: "TheHeader",
+  data() {
+    return {
+      title: "Best Gifter",
+      logo_img: require("../../assets/images/header_logo.png"),
+      icon_img: require("../../assets/images/person-circle.svg"),
+    }
+  }
+}
 </script>
 
 <style scoped>
