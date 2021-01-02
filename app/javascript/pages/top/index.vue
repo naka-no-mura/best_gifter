6b058d80e972f3262bd64cda8b5ab4f3f0fbf9be
naka-no-mura <nakanomura.ryu@gmail.com>
2021-01-02 09:38:19 +0900

    add:top_page

---
+++ app/javascript/pages/top/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+    <h1>Best Gifter</h1>
+    <h2>Wedding</h2>
+    <div class="mt-4">
+      新郎新婦へ、最高のギフトを贈りたい。
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopIndex",
+}
+</script>
+
+<style scoped>
+</style>
