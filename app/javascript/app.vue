85989302b58c3078a3f31ffd3103663901106a63
naka-no-mura <nakanomura.ryu@gmail.com>
2021-01-02 16:23:45 +0900

    add:footer

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -2,18 +2,18 @@
   <div class="d-flex flex-column min-vh-100">
     <TheHeader class="mb-auto" />
     <router-view />
-    <!-- <TheFooter class="mt-auto" /> -->
+    <TheFooter class="mt-auto" />
   </div>
 </template>
 
 <script>
   import TheHeader from 'components/TheHeader.vue'
-  // import TheFooter from 'components/TheFooter.vue'
+  import TheFooter from 'components/TheFooter.vue'
 
   export default {
     components: {
       TheHeader,
-      // TheFooter,
+      TheFooter,
     }
   };
 </script>
