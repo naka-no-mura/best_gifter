1de041d5ce6866c9244abaf4efe366d0dd1599fc
naka-no-mura <nakanomura.ryu@gmail.com>
2021-03-14 13:52:09 +0900

    fix:routes_%i[]

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root 'home#index'
   namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :sessions
-      resources :users do
+      resources :sessions, only: %i[create]
+      resources :users, only: %i[create] do
         collection do
           get 'me'
         end
       end
-      resources :items do
+      resources :items, only: %i[index destroy create] do
         collection do
           get :favorites
         end
@@ -18,13 +18,13 @@ Rails.application.routes.draw do
           get 'search'
         end
       end
-      resources :questionnaires do
+      resources :questionnaires, only: %i[index create destroy] do
         collection do
           get 'my_questionnaires'
         end
       end
-      resources :questionnaire_choices
-      resources :answers
+      resources :questionnaire_choices, only: %i[index create]
+      resources :answers, only: %i[index create]
     end
   end
   get '*path', to: 'home#index'
