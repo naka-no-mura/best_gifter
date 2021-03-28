fb68984bd9a562679ebc23b38a01a115f6f85258
naka-no-mura <nakanomura.ryu@gmail.com>
2021-03-28 19:35:33 +0900

    add:js:true

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Items", type: :system do
+RSpec.describe "Items", type: :system, js: true  do
   let!(:user) { create(:user) }
   let!(:item) { create(:item) }
 
@@ -9,9 +9,9 @@ RSpec.describe "Items", type: :system do
     visit '/items'
   end
 
-  describe '検索機能' do
-    context '全ての入力が正常なとき' do
-      it '検索が成功する' do
+  describe '検索機能', js: true  do
+    context '全ての入力が正常なとき', js: true  do
+      it '検索が成功する', js: true  do
         fill_in 'search-item', with: 'タオル'
         fill_in 'search-min', with: '3000'
         fill_in 'search-max', with: '5000'
@@ -22,8 +22,8 @@ RSpec.describe "Items", type: :system do
       end
     end
 
-    context '価格下限が価格上限よりも大きい時' do
-      it '検索が失敗するので、「検索条件をリセットする」をクリックするとデフォルト検索ができる' do
+    context '価格下限が価格上限よりも大きい時', js: true  do
+      it '検索が失敗するので、「検索条件をリセットする」をクリックするとデフォルト検索ができる', js: true  do
         fill_in 'search-item', with: 'タオル'
         fill_in 'search-min', with: '5000'
         fill_in 'search-max', with: '3000'
@@ -38,8 +38,8 @@ RSpec.describe "Items", type: :system do
       end
     end
 
-    context '検索フォームを空欄で検索した場合' do
-      it 'デフォルト検索が行われる' do
+    context '検索フォームを空欄で検索した場合', js: true  do
+      it 'デフォルト検索が行われる', js: true  do
         fill_in 'search-item', with: ''
         fill_in 'search-min', with: ''
         fill_in 'search-max', with: ''
@@ -50,41 +50,41 @@ RSpec.describe "Items", type: :system do
       end
     end
 
-    describe '検索結果の並び替え' do
-      context 'レビュー件数順' do
-        it '検索が成功する' do
+    describe '検索結果の並び替え', js: true  do
+      context 'レビュー件数順', js: true  do
+        it '検索が成功する', js: true  do
           select 'レビュー件数順', from: '並び替え'
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=-reviewCount&giftFlag=1&imageFlag=1&page=1'
           expect(response.status).to eq(200)
         end
       end
 
-      context 'レビュー評価順' do
-        it '検索が成功する' do
+      context 'レビュー評価順', js: true  do
+        it '検索が成功する', js: true  do
           select 'レビュー評価順', from: '並び替え'
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=-reviewAverage&giftFlag=1&imageFlag=1&page=1'
           expect(response.status).to eq(200)
         end
       end
 
-      context '価格が高い順' do
-        it '検索が成功する' do
+      context '価格が高い順', js: true  do
+        it '検索が成功する', js: true  do
           select '価格が高い順', from: '並び替え'
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=-itemPrice&giftFlag=1&imageFlag=1&page=1'
           expect(response.status).to eq(200)
         end
       end
 
-      context '価格が安い順' do
-        it '検索が成功する' do
+      context '価格が安い順', js: true  do
+        it '検索が成功する', js: true  do
           select '価格が安い順', from: '並び替え'
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=+itemPrice&giftFlag=1&imageFlag=1&page=1'
           expect(response.status).to eq(200)
         end
       end
 
-      context '新着順' do
-        it '検索が成功する' do
+      context '新着順', js: true  do
+        it '検索が成功する', js: true  do
           select '新着順', from: '並び替え'
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=+updateTimestamp&giftFlag=1&imageFlag=1&page=1'
           expect(response.status).to eq(200)
@@ -92,17 +92,17 @@ RSpec.describe "Items", type: :system do
       end
     end
 
-    describe 'ページネーション' do
-      context '最初のページに戻る' do
-        it 'ページ遷移が成功する' do
+    describe 'ページネーション', js: true  do
+      context '最初のページに戻る', js: true  do
+        it 'ページ遷移が成功する', js: true  do
           first('#pg-first').click
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=1'
           expect(response.status).to eq(200)
         end
       end
 
-      context '1ページ前に戻る' do
-        it 'ページ遷移が成功する' do
+      context '1ページ前に戻る', js: true  do
+        it 'ページ遷移が成功する', js: true  do
           first('#pg-number').click
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=2'
           expect(response.status).to eq(200)
@@ -112,24 +112,24 @@ RSpec.describe "Items", type: :system do
         end
       end
 
-      context 'あるページに遷移する' do
-        it 'ページ遷移が成功する' do
+      context 'あるページに遷移する', js: true  do
+        it 'ページ遷移が成功する', js: true  do
           first('#pg-number').click
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=2'
           expect(response.status).to eq(200)
         end
       end
 
-      context '1ページ次に進む' do
-        it 'ページ遷移が成功する' do
+      context '1ページ次に進む', js: true  do
+        it 'ページ遷移が成功する', js: true  do
           first('#pg-next').click
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=2'
           expect(response.status).to eq(200)
         end
       end
 
-      context '最後のページに進む' do
-        it 'ページ遷移が成功する' do
+      context '最後のページに進む', js: true  do
+        it 'ページ遷移が成功する', js: true  do
           first('#pg-last').click
           get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=100'
           expect(response.status).to eq(200)
