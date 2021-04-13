c846961159e5815fdba2cb51cde80daf2cae91f7
naka-no-mura <nakanomura.ryu@gmail.com>
2021-04-13 21:39:17 +0900

    fix:change faile name

--- lib/capistrano/tasks/unicorn.rb
+++ lib/capistrano/tasks/unicorn.rb
@@ -2,9 +2,7 @@
 namespace :unicorn do
   task :environment do
     set :unicorn_pid,    "#{current_path}/tmp/pids/unicorn.pid"
-    # set :unicorn_pid,    "tmp/pids/unicorn.pid"
     set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
-    # set :unicorn_config, "config/unicorn.conf.rb"
   end
 
 #unicornをスタートさせるメソッド
