d0d6b6d515385bce3e6e441fc102e9c1e414e584
naka-no-mura <nakanomura.ryu@gmail.com>
2021-04-13 22:11:32 +0900

    fix:css_faile

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :deploy_to, '/var/www/rails/best_gifter'
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 # シンボリックリンクをはるフォルダ。(※後述)
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'node_modules')
 
 # 保持するバージョンの個数(※後述)
 set :keep_releases, 2
