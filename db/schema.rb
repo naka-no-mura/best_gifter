b95347a1f79bb6f50f7b2ff6a3e5463ed2439ac2
naka-no-mura <nakanomura.ryu@gmail.com>
2021-04-18 10:52:17 +0900

    add:add_index_to_items_table_with_userid_and_itemcode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_015846) do
+ActiveRecord::Schema.define(version: 2021_04_18_014428) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_02_23_015846) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "item_code", null: false
+    t.index ["user_id", "item_code"], name: "index_items_on_user_id_and_item_code", unique: true
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
