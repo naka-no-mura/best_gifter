cd4be0e81b5977d00badf7f383e3a1c1eb918b08
naka-no-mura <nakanomura.ryu@gmail.com>
2021-06-22 14:02:06 +0900

    on_the_way:rakuten_api_webmock_set_up

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,96 +1,31 @@
 require 'rails_helper'
+require 'webmock/rspec'
+include WebMock::API
 
 RSpec.describe "Items", type: :request do
-  describe '検索機能' do
+  before do
+    WebMock.enable!
+  end
+
+  describe ':get api' do
 
     context '全ての入力が正常なとき' do
-      it '検索が成功する' do
-        get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+%E3%82%BF%E3%82%AA%E3%83%AB&genreId=&minPrice=3000&maxPrice=5000&sort=standard&giftFlag=1&imageFlag=1&page=1'
-        json = JSON.parse(response.body)
-        expect(response.status).to eq(200)
-      end
-    end
-
-    context '検索フォームを空欄で検索した場合' do
-      it 'デフォルト検索が行われる' do
-        get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=1'
-        expect(response.status).to eq(200)
-      end
-    end
-
-    describe '検索結果の並び替え' do
-      context 'レビュー件数順' do
-        it '検索が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=-reviewCount&giftFlag=1&imageFlag=1&page=1'
-          expect(response.status).to eq(200)
-        end
-      end
-
-      context 'レビュー評価順' do
-        it '検索が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=-reviewAverage&giftFlag=1&imageFlag=1&page=1'
-          expect(response.status).to eq(200)
-        end
-      end
-
-      context '価格が高い順' do
-        it '検索が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=-itemPrice&giftFlag=1&imageFlag=1&page=1'
-          expect(response.status).to eq(200)
-        end
-      end
-
-      context '価格が安い順' do
-        it '検索が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=+itemPrice&giftFlag=1&imageFlag=1&page=1'
-          expect(response.status).to eq(200)
-        end
-      end
-
-      context '新着順' do
-        it '検索が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=+updateTimestamp&giftFlag=1&imageFlag=1&page=1'
-          expect(response.status).to eq(200)
-        end
-      end
-    end
-
-    describe 'ページネーション' do
-      context '最初のページに戻る' do
-        it 'ページ遷移が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=1'
-          expect(response.status).to eq(200)
-        end
-      end
-
-      context '1ページ前に戻る' do
-        it 'ページ遷移が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=2'
-          expect(response.status).to eq(200)
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=1'
-          expect(response.status).to eq(200)
-        end
-      end
-
-      context 'あるページに遷移する' do
-        it 'ページ遷移が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=2'
-          expect(response.status).to eq(200)
-        end
-      end
-
-      context '1ページ次に進む' do
-        it 'ページ遷移が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=2'
-          expect(response.status).to eq(200)
-        end
-      end
-
-      context '最後のページに進む' do
-        it 'ページ遷移が成功する' do
-          get '/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+&genreId=&minPrice=1000&maxPrice=150000&sort=standard&giftFlag=1&imageFlag=1&page=100'
-          expect(response.status).to eq(200)
-        end
+      it '200 stauts' do
+        stub_request(:get, 'http://localhost:3000/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+%E3%82%BF%E3%82%AA%E3%83%AB&genreId=&minPrice=3000&maxPrice=5000&sort=standard&giftFlag=1&imageFlag=1&page=1').to_return(
+          body: JSON.generate({
+            itemName: 'ギフト 今治タオル 金星 (きんぼし) (-61551-) (t0) 日本製 愛媛今治 木箱入り 今治タオルセット (個別送料込み価格) | タオルギフト 出産内祝い 結婚内祝い 快気祝い 香典返し たおる 国産',
+            itemPrice: 4193,
+            itemUrl: 'https://item.rakuten.co.jp/tabaki/tkd-wi-000019/',
+            mediumImageUrls: [{ imageUrl: 'https://thumbnail.image.rakuten.co.jp/@0_mall/tabaki/cabinet/2019newguideline/tkd-wi-61551-u.jpg?_ex=128x128' }],
+            reviewCount: 4,
+            reviewAverage: 5,
+            shopName: 'たばき ギフト館',
+            genreId: '565606',
+            itemCode: 'tabaki:10311831'
+          }),
+          status: 200
+        )
+        Net::HTTP.get("http://localhost:3000/api/v1/rakuten_apis/search?keyword=%E7%B5%90%E5%A9%9A+%E3%82%BF%E3%82%AA%E3%83%AB&genreId=&minPrice=3000&maxPrice=5000&sort=standard&giftFlag=1&imageFlag=1&page=1", "/")
       end
     end
   end
